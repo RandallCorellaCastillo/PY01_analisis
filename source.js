ed406a12b7a283f5d48e86173dafd1859f7a4b5c
RandallCorellaCastillo <88058459+RandallCorellaCastillo@users.noreply.github.com>
2023-03-24 23:39:40 -0600

    matrix and fill.

--- source.js
+++ source.js
@@ -1,453 +1,159 @@
-matrix = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+//=========================================Globals values====================================================
+/**
+ * @type {String}
+ * Value of null values in matrix.
+ */
+x = "";
+
+/**
+ * @type {int[][]}
+ * Main matrix.
+ * **/
+sudoMatrix = 
+[
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [x, x, x, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, x, x, x],
+  [x, x, x, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, x, x, x],
+  [x, x, x, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, x, x, x],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
-points = 0;
-var terminado = false;
-Valores = [2, 4]
-movements = 0;
-var iniciar = true;
-
-//cronometro
-tiempoRef = Date.now()
-acumulado = 0
+//=========================================End Globals values====================================================
 
 
+/**
+ * @Abstract Clear the matrix.
+ */
+function clearMatrix() {
+  x = "";
+  sudoMatrix = 
+  [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [x, x, x, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, x, x, x],
+    [x, x, x, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, x, x, x],
+    [x, x, x, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, x, x, x],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, x, x, x, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+}
 
-set();
-set();
-drawMatrix();
-drawPoints();
+/**
+ * @abstract search if n is a valid value in this position.
+ * @param x : values of the row.
+ * @param y : values of the column.
+ * @param n : index of the position insert value.
+ * @param mx : max value of x.
+ * @param my : max value of y.
+ * @param n : number to insert.
+ */
+function insertValueXY(x, y, mx, my, n) { 
+  switchValue = true;
+  //=====================================================
+  eq = 0;//value of substract.
+  for (let i = 0; i < 9; i++) { //iter the row.
+    if(mx < x + i) {eq = 9;} // if the row pass the max X. Reset the x.
+    if(sudoMatrix[x + i - eq][y] == n) {switchValue = false;} //if n is in row. put on switch.
+  }
+  //======================================================
+  eq = 0; //reset the value of subs.
+  for (let i = 0; i < 9; i++) { //iter the col
+    if (my < y) {eq = 9;} //if the column pass the max Y. Reset y.
+    if (sudoMatrix[x][y + i - eq] == n) {switchValue = false;} //if n is in column. put on switch.
+  }
+  //======================================================
+  if (switchValue) {sudoMatrix[x][y] = n; return 1;} // if can put n.
+  return -1;//if fail.
+}
 
-window.addEventListener("keydown", function (event) {
-    if (event.defaultPrevented) {
-      return; // Do nothing if the event was already processed
+/**
+ * @abstract fill the 9x9 on the matrix.
+ * @param initX : start X index to fill.
+ * @param initY : start Y index to fill.
+ */
+function fillM(initX, initY) {
+  count = 0;
+  while (!ifFill(initX, initY)) {//if the matrix is not full.
+    YrandomIndex = Math.floor(Math.random() * 9) + initX;
+    XrandomIndex = Math.floor(Math.random() * 9) + initY;
+    randomValue = Math.floor(Math.random() * 9) + 1; // 1-9
+    //console.log("Cords: " + XrandomIndex + YrandomIndex + "==== n: " + randomValue);
+    insertValueXY(XrandomIndex, YrandomIndex, initX + 9, initY + 9, randomValue);
+    count++;
+  }
+  console.log("count: " + count);
+  return -1;
+}
+
+/**
+ * @abstract check status of the matrix.
+ * @param initX : start X index to fill.
+ * @param initY : start Y index to fill.
+ */
+function ifFill(initX, initY) {
+  switchValue = true; 
+  for (let i = initX; i < initX + 9; i++) { // row.
+    for (let j = initY; j < initY + 9; j++) { //col.
+      if(sudoMatrix[i][j] == 0) {switchValue = false;}
     }
+  }
+  return switchValue;
+}
 
-    isFull();
-    if (terminado){
-      end("Game over");
-      return;
-    }
-    let myAudio = document.querySelector('#audio')
-    myAudio.play()
-    cronometer();
-    switch (event.key) {
-      case "ArrowDown":
-        // code for "down arrow" key press.
-        movements++;
-        fixDown();
-        fixDown();
-        fixDown();
-        fixDown();
-        set();
-        drawMatrix();
-        drawPoints();
-        drawMovements();
-        break;
-      case "ArrowUp":
-        // code for "up arrow" key press.
-        movements++;
-        fixUp();
-        fixUp();
-        fixUp();
-        fixUp();
-        set();
-        drawMatrix();
-        drawPoints();
-        drawMovements();
-        break;
-      case "ArrowLeft":
-        // code for "left arrow" key press.
-        movements++;
-        fixLeft();
-        fixLeft();
-        fixLeft();
-        fixLeft();
-        set();
-        drawMatrix();
-        drawPoints();
-        drawMovements();
-        break;
-      case "ArrowRight":
-        movements++;
-        fixRight();
-        fixRight();
-        fixRight();
-        fixRight();
-        set();
-        drawMatrix();
-        drawPoints();
-        drawMovements();
-        // code for "right arrow" key press.
-        break;
-      default:
-        return; // Quit when this doesn't handle the key event.
-    }
-    if (ifWin()) {
-      //
-      terminado = true;
-      end("Win");
-      return;
-    }
-    // Cancel the default action to avoid it being handled twice
-    event.preventDefault();
-  }, true);
 
-   
-//set matrix in html
-function drawMatrix() {
-  //console.log(matrix)
-  var count = 1;
-  for (let x in matrix) {
-    for (let y in matrix) {
-      var text =  matrix[x][y];
-      if (text != 0) {
-        document.getElementById(count).innerHTML = text;
+function printM() {
+  //for (let i = 0; i < 9; i++) {insertValueXY(5 + i, 6, 14, 14, i);}
+  //for (let i = 0; i < 10; i++) {insertValueXY(6, 5 + i, 14, 14, i);} 
+  fillM(6, 6);
+  fillM(0, 0);
+  //fillM(0, 12); 
+  //fillM(12, 0);
+  //fillM(12, 12);
+  //ifFill(6, 6);
+  sudoMatrix[0][12] = "L"
+  //console.log("finish iter");
+
+  var res;
+  for (let i = 0; i < 21; i++) {
+    res+= "\n";
+    for (let j = 0; j < 21; j++) {
+      if(sudoMatrix[i][j] == "") {
+        res += 0;
       } else {
-        document.getElementById(count).innerHTML = "";
+        res += sudoMatrix[i][j];
       }
-      count++;
+      res += " "
     }
   }
-}
-
-function drawPoints() {
-  document.getElementById(0).innerHTML = points;
-}
-
-function drawMovements() {
-  document.getElementById("move").innerHTML = movements;
-}
-
-function functionReset() {
-  clear();
-  set();
-  set();
-  drawPoints();
-  drawMatrix();
-  movements = 0;
-  drawMovements();
-  acumulado = 0;
-  iniciar = false;
-}
-
-function end(text) {
-  if (terminado) {
-    
-    var timeX = document.getElementById("tiempo").textContent;
-    var resF = text + "\n" + "Time:" + timeX +"\nMovements: " +  movements + "   Count of pieces: " + countTotal();
-    document.getElementById("stats").innerHTML = resF;
-    
-    this.document.getElementById("Game Over").style.display = "block";
-
-    /*
-    fillMatrixInit();
-    drawMatrix();
-    */
-    var gameOver = document.getElementById("Game Over");
-    window.onclick = function(event) {
-      if (event.target == gameOver) {
-        gameOver.style.display = "none";
-        clear();
-        set();
-        set();
-        drawMatrix();
-        terminado = false;
-        movements = 0;
-        drawMovements();
-        iniciar = false;
-      }
-    }
-  }
-}
-
-
-// set numbers in empty boxes
-function set() {
-  switchCondition = true;
-  count = 0;
-  while (switchCondition) {
-    firstN = randomNumber(4);
-    secondN = randomNumber(4);
-    number = Valores[randomNumber(2)];
-    if (matrix[firstN][secondN] == 0) {
-      switchCondition = false;
-      matrix[firstN][secondN] = number;
-    }
-    for (let x in matrix) {
-      for (let y in matrix) {
-        if (matrix[x][y] == 0) {
-          count++;
-        }
-      }
-    }
-    if (count == 0) {
-      switchCondition = false;
-    }
-  }
-}
-
-function isFull() {
-  count = 0;
-  for (x = 0; x<4; x++) {
-    for (y = 0; y<4; y++) {
-      if (x < 3) {
-        if (parseInt(matrix[x][y]) == parseInt(matrix[x+1][y])) {
-          count++;
-        }
-        if (parseInt(matrix[x][y]) == parseInt(matrix[x][y+1])) {
-          count++;
-        }
-      }
-      if (matrix[x][y] == 0) {
-        count++;
-      }
-      if (y > 0) {
-        if (parseInt(matrix[x][y]) == parseInt(matrix[x][y-1])) {
-          count++;
-        }
-      }
-      if (y == 3 && x != 3) {
-        if (parseInt(matrix[x][y]) == parseInt(matrix[x+1][y])) {
-          count++;
-        } 
-      }
-    }
-  }
-  if (count == 0) {
-    terminado = true;
-    console.log("No movements");
-  }
-}
-
-//create a rand number.
-function randomNumber(max) {
-  let value = Math.random() * max;
-  var valueF = Math.floor(value);
-  return valueF;
-}
-//fill matrix start game
-function clear() {
-  for (let x in matrix) {
-    for (let y in matrix) {
-      matrix[x][y] = 0;
-    }
-  }
-  document.getElementById(0).innerHTML = 0;
-  points = 0;
-  acumulado = 0;
-}
-
-//finish game 
-function ifWin() {
-  switchW = false;
-  for (let x in matrix) {
-    for (let y in matrix) {
-      if (matrix[x][y] == 2048) {
-        switchW = true;
-      }
-    }
-   }
-  return switchW;
-}
-
-//Count boxes 
-function countTotal() {
-  count = 0;
-  for (let x in matrix) {
-    for (let y in matrix) {
-      count += matrix[x][y]; 
-    }
-   }
-  return count;
-}
-
-// fix the up option
-function fixUp() {
-  for (let x in matrix) {
-    for (let y in matrix) {
-      var text =  matrix[x][y];
-      if (text != 0) { // if the position of matrix is not null
-        //if find a number
-        var switchW = true;
-        count = 1;
-        while(switchW) { //cicle of movements
-          if (x - count >= 0) { //if not index
-            if (matrix[x - count][y] != 0) { //if position is not null
-              
-              if (matrix[x - count][y] == matrix[x - count + 1][y]) {
-                matrix[x - count][y] = (matrix[x - count + 1][y]) * 2;
-                points += (matrix[x - count + 1][y]) * 2;//count of points
-                matrix[x - count + 1][y] = 0;
-                count++;
-                //
-              } else {
-                //close cicle
-                switchW = false;
-
-              }
-              
-            } else { //if the position is null.
-              matrix[x - count][y] = matrix[x - count + 1][y];
-              matrix[(x - count) + 1][y] = 0;
-              count++;
-            }
-          } else{ // if the position is ocuped.
-            switchW = false;
-          }
-        }
-      }
-    }
-  }
-}
-//fix down option+
-function fixDown() {
-  for (let x in matrix) {
-    for (let y in matrix) {
-      var text =  matrix[x][y];
-      if (text != 0) { // if the position of matrix is not null
-        //if find a number
-        var switchW = true;
-        countD = 1;
-        while(switchW) { //cicle of movements
-
-          res = parseInt(countD, 10) + parseInt(x, 10);
-
-          if (res < 4) { //if not index
-            if (matrix[res][y] != 0) { //if position is not null
-              
-              if (matrix[res][y] == matrix[res - 1][y]) {
-                matrix[res][y] = (matrix[res - 1][y]) * 2;
-                points += (matrix[res - 1][y]) * 2; //count of points
-                matrix[res - 1][y] = 0;
-                countD++;
-              } else {
-                //close cicle
-                switchW = false;
-              }
-              
-              countD++;
-            } else { //if the position is null.
-              matrix[res][y] = matrix[res - 1][y];
-              matrix[res - 1][y] = 0;
-              countD++;
-            }
-          } else{ // if the position is ocuped.
-            switchW = false;
-          }
-        }
-      }
-    }
-  }
-}
-
-//fix right option+
-function fixRight() {
-  for (let x in matrix) {
-    for (let y in matrix) {
-      var text =  matrix[x][y];
-      if (text != 0) { // if the position of matrix is not null
-        //if find a number
-        var switchW = true;
-        countD = 1;
-        while(switchW) { //cicle of movements
-
-          res = parseInt(countD, 10) + parseInt(y, 10);
-
-          if (res < 4) { //if not index
-            if (matrix[x][res] != 0) { //if position is not null
-              
-              if (matrix[x][res] == matrix[x][res - 1]) {
-                matrix[x][res] = (matrix[x][res - 1]) * 2;
-                points += (matrix[x][res - 1]) * 2;
-                matrix[x][res - 1] = 0;
-                countD++;
-              } else {
-                //close cicle
-                switchW = false;
-              }
-              
-              countD++;
-            } else { //if the position is null.
-              matrix[x][res] = matrix[x][res - 1];
-              matrix[x][res - 1] = 0;
-              countD++;
-            }
-          } else{ // if the position is ocuped.
-            switchW = false;
-          }
-        }
-      }
-    }
-  }
-}
-
-//fix left option+
-function fixLeft() {
-  for (let x in matrix) {
-    for (let y in matrix) {
-      var text =  matrix[x][y];
-      if (text != 0) { // if the position of matrix is not null
-        //if find a number
-        var switchW = true;
-        countD = 1;
-        while(switchW) { //cicle of movements
-
-          res = parseInt(y, 10) - parseInt(countD, 10);
-          if (res >= 0) { //if not index
-            if (matrix[x][res] != 0) { //if position is not null
-              if (matrix[x][res] == matrix[x][res + 1]) {
-                matrix[x][res] = (matrix[x][res + 1]) * 2;
-                points += (matrix[x][res + 1]) * 2;
-                matrix[x][res + 1] = 0;
-                countD++;
-              } else {
-                //close cicle
-                switchW = false;
-              }
-              
-              countD++;
-            } else { //if the position is null.
-              matrix[x][res] = matrix[x][res + 1];
-              matrix[x][res + 1] = 0;
-              countD++;
-            }
-          } else{ // if the position is ocuped.
-            switchW = false;
-          }
-        }
-      }
-    }
-  }
-}
-
-//creditos:  https://www.youtube.com/watch?v=EkS5AIqednw&t=243s
-function cronometer() {
-  iniciar = true;
-  setInterval(() => {
-    tiempo = document.getElementById("tiempo")
-    if (iniciar) {
-        acumulado += Date.now() - tiempoRef
-    }
-    tiempoRef = Date.now()
-    tiempo.innerHTML = formatear(acumulado)
-  }, 1000/60)
-
-  function formatear(tiempo_ms) {
-    MS = tiempo_ms % 1000    
-    St = Math.floor(((tiempo_ms - MS) / 1000))
-    S = St%60
-    M = Math.floor((St / 60) % 60)
-    H = Math.floor((St/60 / 60))
-    Number.prototype.ceros = function (n) {
-        return (this + "").padStart(n, 0)
-    }
-
-    return H.ceros(2) + ":" + M.ceros(2) + ":" + S.ceros(2)
-        + "." + MS.ceros(3)
-  }
-
+  console.log(res);
+  
 }
